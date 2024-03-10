99f05172f0c3b98b92559ba61c13cb133892dc19
K\K <kwonabraham@naver.com>
2024-03-10 16:51:30 +0900

    MemberService 생성

--- src/main/java/com/study/erum/controller/MemberController.java
+++ src/main/java/com/study/erum/controller/MemberController.java
@@ -2,7 +2,11 @@ package com.study.erum.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.study.erum.dto.MemberDTO;
 
 @Controller
 @RequestMapping("/member")
@@ -11,5 +15,10 @@ public class MemberController {
   @GetMapping("/save")
   public String saveForm(){
     return "save";
+  }
+  
+  @PostMapping("/save")
+  public String save(@ModelAttribute MemberDTO memberDTO){
+    
   }
 }
--- src/main/java/com/study/erum/dto/MemberDTO.java
+++ src/main/java/com/study/erum/dto/MemberDTO.java
@@ -7,6 +7,8 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+
+//DTO 변수와 save.jsp에 있는 name이 반드시 일치해야 @ModelAttribue를 사용 가능.
 public class MemberDTO {
   private Long id;
   private String memberEmail;
