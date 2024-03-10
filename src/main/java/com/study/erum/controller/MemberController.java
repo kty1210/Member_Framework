ef40f25959b8801d183cac29e2e959ffd2fe6cc7
K\K <kwonabraham@naver.com>
2024-03-10 23:37:18 +0900

    로그아웃 구현

--- src/main/java/com/study/erum/controller/MemberController.java
+++ src/main/java/com/study/erum/controller/MemberController.java
@@ -106,4 +106,9 @@ public class MemberController {
     String checkResult = memberService.emailCheck(memberEmail);
     return checkResult;
   }
+  
+  @GetMapping("/logout")
+  public String logout(){
+    return "logout";
+  }
 }
